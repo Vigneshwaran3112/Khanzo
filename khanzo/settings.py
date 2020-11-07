496943835fe6bf957cd9c8928d5105b32b8615c5
Vigneshwaran3112 <57023786+Vigneshwaran3112@users.noreply.github.com>
2020-11-08 02:02:48 +0530

    Update settings.py

--- khanzo/settings.py
+++ khanzo/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, '/food/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/food/static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/food/static/'),
+    os.path.join(BASE_DIR, '/food/static'),
 )
 
 
