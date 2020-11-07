e184a01256c117b34aec969fccd6c917c51ae18b
Vigneshwaran3112 <57023786+Vigneshwaran3112@users.noreply.github.com>
2020-11-08 02:01:57 +0530

    Update settings.py

--- khanzo/settings.py
+++ khanzo/settings.py
@@ -120,8 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '/food/static/')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '/food/static/'),
+)
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
